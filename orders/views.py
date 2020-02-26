7011a043d1d806d4c2f81962b5b6c7cd42ab53a8
cavesdev <24899540+cavesdev@users.noreply.github.com>
2020-02-26 11:58:30 -0600

    - Updated database with new user. - Created register and login forms. - Implemented user registration and authentication.

--- orders/views.py
+++ orders/views.py
@@ -1,10 +1,13 @@
-from django.http import HttpResponse
-from django.shortcuts import render
+from django.contrib.auth import authenticate, logout, login
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect
 
 from .models import Pizza, Sub, Pasta, Salad, DinnerPlatter
+from .forms import RegisterForm, AuthenticationForm
 
 
 # Create your views here.
+@login_required(login_url='login')
 def index(request):
     context = {
         'pizzas': Pizza.objects.all(),
@@ -14,3 +17,39 @@ def index(request):
         'platters': DinnerPlatter.objects.all()
     }
     return render(request, 'orders/index.html', context)
+
+
+def register(request):
+    if request.method == 'POST':
+        form = RegisterForm(request.POST)
+
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
+
+    form = RegisterForm()
+    return render(request, 'orders/register.html', {'form': form})
+
+
+def log_in(request):
+    if request.user.is_authenticated:
+        return redirect('index')
+
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+        else:
+            return redirect('login')
+        return redirect('index')
+        
+    form = AuthenticationForm()
+    return render(request, 'orders/login.html', {'form': form})
+
+
+def log_out(request):
+    logout(request)
+    return redirect('index')
